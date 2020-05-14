b3b1f04200fd3cb97bb2f186d35199810f2dea0b
meaghan jennings <meaghanljennings@gmail.com>
2020-05-14 23:40:13 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+  
+display_board.rb defines a method display_board
+ 
+def display_board
+puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
